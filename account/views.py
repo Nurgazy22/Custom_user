71736333dad1ced5d16c9c1a99e25ce518230908
Nurgazy <amanbekovnurgazy@mail.ru>
2023-01-09 12:35:19 +0600

    first commit

---
+++ account/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import RegistrationSerializer, LoginSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+
+class RegistrationView(APIView):
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response('Аккаунт успешно создан', status=201)
+
+
+class LoginView(ObtainAuthToken):
+    serializer_class = LoginSerializer
